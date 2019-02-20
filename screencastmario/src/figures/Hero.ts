b77574f49f805ac6a7e26cd8f2d041b221db1f71
phongvu99 <phongvu99@outlook.com>
2019-02-20 23:39:57 +0700

    first task

--- screencastmario/src/figures/Hero.ts
+++
@@ -1,8 +0,0 @@
-import {Figure} from './Figure';
-import {Level} from '../engine/Level';
-
-export class Hero extends Figure {
-    constructor(x: number, y: number, level: Level) {
-        super(x, y, level);
-    }
-}
--- screencastmario/src/items/ItemFigure.ts
+++
@@ -1,8 +0,0 @@
-import {Level} from '../engine/Level';
-import {Figure} from '../figures/Figure';
-
-export class ItemFigure extends Figure {
-    constructor(x: number, y: number, level: Level) {
-        super(x, y, level);
-    }
-}
