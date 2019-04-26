930e167189b1afccdfaf33e3c5642847e7c9853f
MidoDoan <huongdl.hx@gmail.com>
2019-04-26 13:27:35 +0700

    Update Demo.class

--- todo/src/main/java/Demo.java
+++ todo/src/main/java/Demo.java
@@ -1,8 +1,15 @@
+import controller.Controller;
 import view.View;
+import model.List;
 
 public class Demo {
-    public static void main(String[] args){
-        View view = new View();
+    public static void main(String[] args) {
+        List myList = new List();
+        myList.showList();
+        View view = new View(myList);
+        Controller controller = new Controller(myList, view);
+        controller.setListener();
+        controller.updateView(true);
     }
 }
 
