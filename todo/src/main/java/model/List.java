d9df57312ac2d8aee6b621cef45ddad92b4a0eeb
MidoDoan <huongdl.hx@gmail.com>
2019-04-26 08:33:17 +0700

    Implement List class to contain tasks

--- todo/src/main/java/model/List.java
+++ todo/src/main/java/model/List.java
@@ -1,4 +1,12 @@
 package model;
 
-public class List {
+import java.util.ArrayList;
+
+public class List extends ArrayList<Task> {
+    public void showList() {
+        System.out.println("List Info:");
+        for (int i = 0; i < this.size(); i++) {
+            System.out.println(this.get(i).getContent() + "/" + this.get(i).getState());
+        }
+    }
 }
